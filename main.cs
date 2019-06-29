c2df84436c16cad588d52743a8261f06c4bfcd53
ChamodyaDias <chamodyadias218@gmail.com>
2019-06-29 14:47:25 +0530

    added more animation

--- main.cs
+++ main.cs
@@ -6,7 +6,7 @@ function BoatModule::create( %this )
     exec("./scripts/mainplane.cs");
     exec("./scripts/ocean.cs");
     exec("./scripts/sky.cs");
-    
+
     echo("Hello World!");
 
     // createSceneWindow();
@@ -14,10 +14,11 @@ function BoatModule::create( %this )
     // mySceneWindow.setScene(SandboxScene);
     SandboxScene.setDebugOn("collision", "position", "aabb");
     // Set the scene gravity.
-    SandboxScene.setGravity( 0, -0.8 );
+    SandboxScene.setGravity(0, -9.8);
 
 
     BoatModule.MainPlane = createMainPlane();
+    BoatModule.isTouchDown = false;
 
     // createOcean();
     // createSky();
@@ -31,5 +32,22 @@ function BoatModule::destroy( %this )
 function BoatModule::onTouchDown(%this, %touchID, %worldPosition)
 {
     echo("clicked!");
-    BoatModule.MainPlane.Position.y += "5";
+    BoatModule.LiftUpPlane(%this);
+}
+
+function BoatModule::onTouchUp(%this, %touchID, %worldPosition)
+{
+    echo("click released");
+    BoatModule.StopLiftUpPlane(%this);
+}
+
+
+function BoatModule::LiftUpPlane(%this){
+    BoatModule.MainPlane.setLinearVelocity(0,10);
+     %this.thrustschedule = %this.schedule(100,LiftUpPlane);
+}
+
+function BoatModule::StopLiftUpPlane(%this){
+    BoatModule.MainPlane.setLinearDamping(0.8);
+    cancel(%this.thrustschedule);
 }
